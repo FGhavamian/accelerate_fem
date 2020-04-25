2b720fecef9e73493e7f0aa680f67596c10deaef
Fariborz Ghavamian <fari.ghavamian@gmail.com>
2020-04-25 19:08:54 +0200

    data generation pipeline

---
+++ accelerate_simulations/geometry/__init__.py
@@ -0,0 +1,2 @@
+from .abstract_geometry import AbstractGeometry
+from .meshed_geometry import make_msh
---
+++ scripts/generate_data.py
@@ -0,0 +1,75 @@
+import os
+import sys
+import pickle
+
+sys.path.append(os.path.join(os.getcwd(), '..'))
+
+from accelerate_simulations.geometry import AbstractGeometry, make_msh
+
+
+b_1 = [100.0]
+y_1 = [10.0]
+
+b_2 = [100.0]
+y_2 = [1.0]
+
+circle_location_seed_list = [1]
+n_circles_list = [3]
+circle_radius_list = [10.0]
+box_size_list = [(100, 100)]
+
+path_data_dir = os.path.join(os.getcwd(), '..', 'data', 'raw')
+
+path_fem = os.path.join(os.getcwd(), '..', 'fem', 'build', 'fem')
+if not os.path.exists(path_fem): raise Exception('build fem first')
+
+
+def make_directories_for(case_name):
+    path_case_dir = os.path.join(path_data_dir, case_name)
+
+    if os.path.exists(path_case_dir): 
+        raise Exception(f'{path_case_dir} exists, remove it manually')
+
+    paths = {
+        'mesh': os.path.join(path_case_dir, 'mesh'),
+        'mesh_hex': os.path.join(path_case_dir, 'mesh_hex.msh'), 
+        'abstract_geometry':  os.path.join(path_case_dir, 'abstract_geometry.pickle'), 
+        'solution_dir': os.path.join(path_case_dir, 'solution/'), 
+        'plastic_strain_dir': os.path.join(path_case_dir, 'plastic_strain/')
+    }
+
+    os.makedirs(path_case_dir)
+    os.makedirs(paths['solution_dir'])
+    os.makedirs(paths['plastic_strain_dir'])
+
+    return paths
+
+
+for i, circle_location_seed in enumerate(circle_location_seed_list):
+    case_name = str(circle_location_seed)
+    
+    paths = make_directories_for(case_name)
+
+    abstract_geometry = AbstractGeometry(
+        n_circles_list[0],
+        circle_radius_list[0],
+        box_size_list[0],
+        circle_location_seed)
+
+    abstract_geometry.save_at(paths['abstract_geometry'])
+
+    make_msh(paths['mesh'], abstract_geometry, cl_coarse=10.0, cl_fine=5.0, verbose=False)
+    
+    fem_args = [
+        paths['mesh_hex'],
+        paths['solution_dir'],
+        paths['plastic_strain_dir'],
+        str(b_1[0]),
+        str(y_1[0]),
+        str(b_2[0]),
+        str(y_2[0])
+    ] 
+
+    fem_args = ' '.join(fem_args)
+    command = f'{path_fem} {fem_args}'
+    os.system(command)
